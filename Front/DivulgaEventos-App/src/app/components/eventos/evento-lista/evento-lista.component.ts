3248be6e168867876fce57cf22036c45d5760838
rodrigo barreto <rodrigo_.barreto@hotmail.com>
2023-09-20 11:30:19 -0300

    criando rotas login, registration

--- Front/DivulgaEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ Front/DivulgaEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
@@ -45,7 +46,8 @@ export class EventoListaComponent implements OnInit {
     private eventoService: EventoService,
     private modalService: BsModalService,
     private toastr: ToastrService,
-    private spinner: NgxSpinnerService){}
+    private spinner: NgxSpinnerService,
+    private router: Router){}
 
   public ngOnInit(): void {
     this.spinner.show();
@@ -86,5 +88,8 @@ export class EventoListaComponent implements OnInit {
     this.modalRef?.hide();
   }
 
+  detalheEvento(id: number): void {
+    this.router.navigate([`eventos/detalhe/${id}`]);
+  }
 
 }
